66b3d4c23eed6d7cb0d8dc7b0c935ae2436a3953
Surihix <neweramysterymailer@gmail.com>
2024-11-10 03:31:53 +0530

    added encryption shuffle data function

--- FFXVSaveCrypt/Crypto/Encrypt.cs
+++ FFXVSaveCrypt/Crypto/Encrypt.cs
@@ -46,12 +46,12 @@ namespace FFXVSaveCrypt.Crypto
                 }
             }
 
-            File.WriteAllBytes("enc_1", encryptedData);
+            //File.WriteAllBytes("enc_1", encryptedData);
 
 
             // Shuffle the encrypted data
             ShuffleData(cryptoVars, encryptedData);
-            File.WriteAllBytes("enc_2", encryptedData);
+            //File.WriteAllBytes("enc_2", encryptedData);
 
             // Tweak the encrypted data
             var tweakBytesList = new List<byte>();
@@ -92,12 +92,36 @@ namespace FFXVSaveCrypt.Crypto
             var bufferSize = encryptedData.Length >> 4;
             var keyStack = SharedFunctions.GenerateKeyStack(cryptoVars, bufferSize);
 
-            for (var shuffleIterator = 1; shuffleIterator < bufferSize; shuffleIterator++)
+            for (var shuffleIterator = bufferSize - 1; shuffleIterator > 0; shuffleIterator--)
             {
                 var destination = new List<ulong>();
                 var source = new List<ulong>();
 
+                int destStart = 16 * shuffleIterator;
+                destination.Add(BitConverter.ToUInt64(encryptedData, destStart));
+                destination.Add(BitConverter.ToUInt64(encryptedData, destStart + 8));
 
+                int sourceStart = 16 * keyStack[shuffleIterator];
+                source.Add(BitConverter.ToUInt64(encryptedData, sourceStart));
+                source.Add(BitConverter.ToUInt64(encryptedData, sourceStart + 8));
+
+                ulong oldDestinationVal;
+                ulong oldSourceVal;
+
+                oldDestinationVal = destination[0];
+                oldSourceVal = source[0];
+                destination[0] = oldSourceVal;
+                source[0] = oldDestinationVal;
+
+                oldDestinationVal = destination[1];
+                oldSourceVal = source[1];
+                destination[1] = oldSourceVal;
+                source[1] = oldDestinationVal;
+
+                BitConverter.GetBytes(destination[0]).CopyTo(encryptedData, destStart);
+                BitConverter.GetBytes(destination[1]).CopyTo(encryptedData, destStart + 8);
+                BitConverter.GetBytes(source[0]).CopyTo(encryptedData, sourceStart);
+                BitConverter.GetBytes(source[1]).CopyTo(encryptedData, sourceStart + 8);
             }
         }
     }
